378f95cd682215bec4c1f26dd20d7928166ba34b
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2012-10-26 07:55:41 +0200

    Parse JSON, play ogg file.

--- src/com/mahmoudhossam/sayit/MainActivity.java
+++ src/com/mahmoudhossam/sayit/MainActivity.java
@@ -2,7 +2,13 @@ package com.mahmoudhossam.sayit;
 
 import java.util.concurrent.ExecutionException;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -30,7 +36,13 @@ public class MainActivity extends Activity {
 		EditText textBox = (EditText) findViewById(R.id.editText1);
 		String text = textBox.getText().toString();
 		String result = new MakeRequest().execute(text).get();
-		
+		String url = new ParseJSON().execute(result).get();
+		Uri uri = Uri.parse(url);
+		MediaPlayer player = MediaPlayer.create(getApplicationContext(), uri);
+		if (player.isPlaying()){
+			player.stop();
+		}
+		player.start();
 	}
 
 	class MakeRequest extends AsyncTask<String, Integer, String> {
@@ -38,6 +50,23 @@ public class MainActivity extends Activity {
 		@Override
 		protected String doInBackground(String... arg0) {
 			return api.get_std_pronouncation(arg0[0]);
+		}
+
+	}
+
+	class ParseJSON extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			JSONTokener tokener = new JSONTokener(params[0]);
+			try {
+				JSONObject root = (JSONObject) tokener.nextValue();
+				JSONObject contents = (JSONObject) root.getJSONArray("items").get(0);
+				return contents.getString("pathogg");
+			} catch (JSONException e) {
+				System.err.println(e.getMessage());
+			}
+			return null;
 		}
 
 	}
